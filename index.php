8deaa4a886639c484da3f15b8008145f564bf291
Emir <emirsoyalan79@gmail.com>
2022-10-10 14:47:00 +0300

    Rename title

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-    <title>Document</title>
+    <title>SOS</title>
     <script
     src="https://code.jquery.com/jquery-3.6.1.min.js"
     integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
