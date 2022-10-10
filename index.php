7a6a0b1c1746757136845b25e20a399eaff08ab5
Emir <emirsoyalan79@gmail.com>
2022-10-10 15:19:32 +0300

    Add dynamic input

--- index.php
+++ index.php
@@ -9,23 +9,52 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-    $n = 10;
-    $m = 10;
-    ?>
 
     <div id="gameArea">
         <canvas id="canvas"></canvas>
         <table id="gameTable">
             <?php
-            $idCounter = 0;
-            for ($i = 0; $i < $n; $i++) {
-                echo "<tr>";
-                for ($j = 0; $j < $m; $j++) {
-                    echo "<td data-row=".$i." data-col=".$j." id='t".$idCounter."' onclick='tdclick(this)'>&nbsp</td>";
-                    $idCounter += 1;
+            if (!isset($_GET['n']) || !isset($_GET['m'])) {
+                echo "
+                <tr>
+                    <td>
+                        <form action='index.php' method='get'>
+                            <input type='text' name='n' placeholder='n'/>
+                            <br />
+                            <br />
+                            <input type='text' name='m' placeholder='m'/>
+                            <br />
+                            <br />
+                            <input style='width: 170px' type='submit' value='Start'/>
+                        </form>
+                    </td>
+                </tr>
+                ";
+            } else {
+                $n = $_GET['n'];
+                $m = $_GET['m'];
+
+                // sanitize input
+                try{
+                    $n = intval($n);
+                    $m = intval($m);
+                    if ($n == 0 || $m == 0)
+                        throw new Exception("Invalid input");
+                } catch (Exception $e) {
+                    echo $e->getMessage();
+                    return;
                 }
-                echo "</tr>";
+
+                // create table
+                $idCounter = 0;
+                for ($i = 0; $i < $n; $i++) {
+                    echo "<tr>";
+                    for ($j = 0; $j < $m; $j++) {
+                        echo "<td data-row=".$i." data-col=".$j." id='t".$idCounter."' onclick='tdclick(this)'>&nbsp</td>";
+                        $idCounter += 1;
+                    }
+                    echo "</tr>";
+                }
             }
             ?>
         </table>
