6a62946f025e023e147f8a972f498e813aa0699d
Emir <emirsoyalan79@gmail.com>
2022-10-11 19:28:49 +0300

    Bugfix && endgame && draw lines

--- main.js
+++ main.js
@@ -47,14 +47,10 @@ function drawVerticalLine(rect){
     ctx.lineTo(middleX, rect.bottom);
     ctx.stroke();
 }
-function drawDoubleLine(rect){
-    drawVerticalLine(rect, rect.top, rect.bottom);
-    drawHorizontalLine(rect, rect.left, rect.right);
-}
 
 function changeText(){
-    newText = (selectedText == "S") ? "O" : "S";
-    document.getElementById("selectedText").innerHTML = newText;
+    selectedText = (selectedText == "S") ? "O" : "S";
+    document.getElementById("selectedText").innerHTML = selectedText;
 }
 
 function undoMove(){
@@ -79,35 +75,39 @@ function tdclick(elem){
 
     // player move
     elem.innerHTML = selectedText;
-    /* actionHistory.push(
-        {
-            "row": elem.dataset.row,
-            "col": elem.dataset.col,
-            "text": selectedText,
-            "id": elem.id,
-            "rect": elem.getBoundingClientRect()
-        }
-    ); */
     
     // check soses found by player
     for(var i = 0; i < potential_soses.length; i++){
-        if(potential_soses[i][1] == elem.id.substring(1)){
+        var sos_id = potential_soses[i][1];
+        var elem_id = elem.id.substring(1);
+        if(sos_id == elem_id){
+            var coordinates = potential_soses[i][0];
+            drawLine(coordinates);
             blueScore += 1;
             document.getElementById("blue").innerHTML = 'Blue Team Score: ' + blueScore;
             return;
         }
     }
 
-    changeTurn();
-    
     // AI move
+    changeTurn();
     soses = findSOSes();
     while(soses.length!=0){
         placeSoses(soses);
         soses = findSOSes();
     }
-    
-    makeRandomMove();
+    emptyCount = countEmpties();
+    if(emptyCount == 0){
+        console.log("Game Over");
+        return;
+    }
+    else if(emptyCount == 1){
+        makeRandomMove();
+        console.log("Game Over");
+    }
+    else{
+        makeRandomMove();
+    }
 };
 
 function placeSoses(soses){
@@ -117,6 +117,7 @@ function placeSoses(soses){
         text = sos[2];
         elem = document.getElementById('t'+id);
         elem.innerHTML = text;
+        drawLine(coordiantes);
         redScore += 1;
         document.getElementById("red").innerHTML = 'Red Team Score: ' + redScore;
     }
@@ -133,7 +134,6 @@ function findSOSes(){
                 val = document.getElementById('t'+(id+2)).innerHTML
                 if (val == 'S'){
                     soses.push([[id, id+1, id+2], id+1, 'O']);
-                    id=id+1;
                 }
             }
         } 
@@ -143,7 +143,6 @@ function findSOSes(){
                 val = document.getElementById('t'+(id+2)).innerHTML
                 if (val == 'S'){
                     soses.push([[id, id+1, id+2], id, 'S']);
-                    id=id+1;
                 }
             }
         } 
@@ -153,11 +152,10 @@ function findSOSes(){
                 val = document.getElementById('t'+(id+2)).innerHTML;
                 if (val == '&nbsp;'){
                     soses.push([[id, id+1, id+2], id+2, 'S']);
-                    id=id+1;
                 }
             }
         }
-        
+
         if(m == 3){
             id += 2;
         }
@@ -212,7 +210,7 @@ function findSOSes(){
 }
 
 function makeRandomMove(){
-    var randomId = Math.floor(Math.random() * (n*m));
+    var randomId = Math.floor(  Math.random() * (n*m));
     var randomText = (Math.random() < 0.5) ? "S" : "O";
     elem = document.getElementById('t'+randomId);
     while(elem.innerHTML != "&nbsp;"){
@@ -221,3 +219,50 @@ function makeRandomMove(){
     }
     elem.innerHTML = randomText;
 }
+
+function isGameOver(){
+    // check if there is any empty cell
+    for(var i = 0; i < n*m; i++){
+        elem = document.getElementById('t'+i);
+        if(elem.innerHTML == "&nbsp;")
+            return false;
+    }
+    return true;
+}
+
+function drawLine(coordinates){
+    function getDirection(){
+        if(coordinates[0] == coordinates[1]-1)
+            return "horizontal";
+        else
+            return "vertical";
+    }
+    // foreach coordinate
+    for(var j = 0; j < coordinates.length; j++){
+        var rect = document.getElementById("t" + coordinates[j]).getBoundingClientRect();
+        if(getDirection() == "horizontal")
+            drawHorizontalLine(rect);
+        else
+            drawVerticalLine(rect);
+    }
+}
+
+function countEmpties(){
+    var count = 0;
+    for(var i = 0; i < n*m; i++){
+        elem = document.getElementById('t'+i);
+        if(elem.innerHTML == "&nbsp;")
+            count++;
+    }
+    return count;
+}
+
+/* actionHistory.push(
+            {
+                "row": elem.dataset.row,
+                "col": elem.dataset.col,
+                "text": selectedText,
+                "id": elem.id,
+                "rect": elem.getBoundingClientRect()
+            }
+        ); */
